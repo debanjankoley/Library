a5fd0734e939a8dca7d8b4e34b1a7b71027d0700
Debanjan Koley <81432461+debanjankoley@users.noreply.github.com>
2024-06-13 13:27:24 +0530

    add structure to html table, add minimal css to html table, add a function to loop through the array to display the books on the page

--- main.js
+++ main.js
@@ -8,8 +8,25 @@ function Book(name, author, pages, status) {
 }
 
 function addBookToLibrary(title, author, pages, status) {
-    let book = new Book(title, author, pages, status)
-    myLibrary.push(book)
+    let book = new Book(title, author, pages, status);
+    myLibrary.push(book);
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295', 'read')
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295', 'read');
+addBookToLibrary('Harry Potter', 'J.K Rowling', '800', 'read');
+
+const tableBody = document.querySelector('tbody');
+
+function updateDisplay () {
+    myLibrary.forEach(book => {
+        const tr = document.createElement('tr');
+        tableBody.appendChild(tr);
+        Object.values(book).forEach(val => {
+            const td = document.createElement('td');
+            td.textContent = val;
+            tr.appendChild(td);
+        });
+    });
+};
+
+updateDisplay();
