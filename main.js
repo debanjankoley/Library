4edffa3e2f040ae330ca5cb29f1efcfdddfa8e63
Debanjan Koley <81432461+debanjankoley@users.noreply.github.com>
2024-06-13 20:00:57 +0530

    add a new book button that brins up a form allowing users to input details for new book and then update the display

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const myLibrary = []
 
-function Book(name, author, pages, status) {
-    this.name = name;
+function Book(title, author, pages, status) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.status =  status;
@@ -13,16 +13,20 @@ function addBookToLibrary(title, author, pages, status) {
 }
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295', 'read');
-addBookToLibrary('Harry Potter', 'J.K Rowling', '800', 'read');
+addBookToLibrary('Harry Potter', 'J.K Rowling', '800', 'not read');
 
-const tableBody = document.querySelector('tbody');
+const tableBody = document.querySelector("tbody");
 
 function updateDisplay () {
+    while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.lastChild);
+    };
+
     myLibrary.forEach(book => {
-        const tr = document.createElement('tr');
+        const tr = document.createElement("tr");
         tableBody.appendChild(tr);
         Object.values(book).forEach(val => {
-            const td = document.createElement('td');
+            const td = document.createElement("td");
             td.textContent = val;
             tr.appendChild(td);
         });
@@ -30,3 +34,28 @@ function updateDisplay () {
 };
 
 updateDisplay();
+
+const showButton = document.getElementById("showDialog");
+const dialog = document.querySelector("dialog");
+const titleBox = document.getElementById("title");
+const authorBox = document.getElementById("author");
+const pagesBox = document.getElementById("pages");
+const statusBox = document.getElementById("status");
+const form = document.querySelector("form");
+
+showButton.addEventListener("click", () => {
+    form.reset();
+    dialog.showModal();
+});
+
+dialog.addEventListener("close", () => {
+    statusBox.value = statusBox.checked ? "read" : "not read" ;
+
+    if (dialog.returnValue === "cancel") {
+        return;
+    } else if (dialog.returnValue === "confirm") {
+        console.log("hello")
+        addBookToLibrary(titleBox.value, authorBox.value, pagesBox.value, statusBox.value);
+        updateDisplay();
+    };
+});
